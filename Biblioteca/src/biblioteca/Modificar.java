6251bb69938e71c76ab7dd52671afa171bd32902
Aketzali-25 <aketluna.2506@gmail.com>
2024-02-07 23:46:55 -0600

    Se establecio la conexion en todas los funcionamientos de agregar, consultar, modificar, vender

--- Biblioteca/src/biblioteca/Modificar.java
+++ Biblioteca/src/biblioteca/Modificar.java
@@ -45,7 +45,7 @@ public class Modificar extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ClassNotFoundException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -70,6 +70,7 @@ public class Modificar extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
+         Conexion.conectar();
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Modificar().setVisible(true);
